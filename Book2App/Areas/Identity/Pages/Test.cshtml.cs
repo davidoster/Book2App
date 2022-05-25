4ae26dc2e1a7f083352ee40435626b29bde35d1f
EustaceKr <efstathios.kravaritis@gmail.com>
2022-05-25 19:51:56 +0300

    "Task"

--- Book2App/Areas/Identity/Pages/Test.cshtml.cs
+++ Book2App/Areas/Identity/Pages/Test.cshtml.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace Book2App.Areas.Identity.Pages
 {
     public class TestModel : PageModel
     {
-        string ID;
-        public void OnGet()
+        [BindProperty]
+        public string TestData { get; set; }
+
+        public IActionResult OnPost()
         {
+            return RedirectToAction("TestMethod", "Test", new {data = TestData });
         }
     }
 }
